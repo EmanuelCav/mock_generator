9928ab973e95ca593bd1f4fc92f0817b66c735b0
Emanuel Cavallin <cavallinema@gmail.com>
2026-09-09 17:23:42 -0300

    Modify permissions and splash

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,13 +1,15 @@
 import { View } from 'react-native'
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
-import { homeStyles } from '../styles/home.styles';
-
 const adUnitId = __DEV__ ? TestIds.BANNER : `${process.env.EXPO_PUBLIC_BANNER}`;
 
 const Banner = () => {
     return (
-        <View style={homeStyles.containerBanner}>
+        <View style={{
+            height: '8%',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
+        }}>
             <BannerAd
                 unitId={adUnitId as string}
                 size={BannerAdSize.BANNER}
